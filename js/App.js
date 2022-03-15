0d50aae96f18c2e2ef5624b17695d3d3e948e76c
Aryan Sethi <aryan.rdps@gmail.com>
2022-03-15 13:24:45 +0530

    Completed

--- js/App.js
+++ js/App.js
@@ -1,11 +1,15 @@
 // DOM elements
 let arrow = document.querySelectorAll(".arrow");
-let sidebar = document.querySelector(".sidebar"); 
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
 
 for (const i of arrow) {
-    i.addEventListener("click" , (e)=>{
+    i.addEventListener("click", e => {
         let arrowParent = e.target.parentElement.parentElement;
         arrowParent.classList.toggle("showMenu");
     });
 }
-console.log(sidebar);
+
+sidebarBtn.addEventListener("click", e => {
+    sidebar.classList.toggle("close");
+});
